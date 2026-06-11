e15af1cdbbf8356b63437cf5ee8f726d4e1ab08b
rozi <rozichilwal09@gmail.com>
2026-06-11 10:11:34 +0530

    integrated AI functionality

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -16,9 +16,11 @@ app.use(cors({
 
 // require all the auth routes here
 const authRouter = require("./routes/auth.routes");
+const interviewRouter = require("./routes/interview.routes")
 
 // using all routes here
 app.use("/api/auth",authRouter);
+app.use("/api/interview", interviewRouter)
 
 
 
